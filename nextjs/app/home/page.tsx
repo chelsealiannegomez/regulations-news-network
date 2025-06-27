44a30bb50f01866675d9c940283e5225f675485d
Chelsea Lianne Tan Gomez <chelseagomez@meta.com>
2025-06-27 13:17:29 -0700

    resolved Rucha's comments, fixed user null type in home page

--- nextjs/app/home/page.tsx
+++ nextjs/app/home/page.tsx
@@ -1,39 +1,36 @@
-import { cookies } from 'next/headers';
-import { decrypt } from '@/lib/session';
-import { redirect } from 'next/navigation';
-import prisma from '@/lib/prisma';
-import HomePage from '../components/home/HomePage';
-import { User } from '@/lib/definitions'
+import { cookies } from "next/headers";
+import { decrypt } from "@/lib/session";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
+import HomePage from "../components/home/HomePage";
 
-
-export default async function Page () {
-    const sessionCookie = (await cookies()).get('session')?.value;
+export default async function Page() {
+    const sessionCookie = (await cookies()).get("session")?.value;
 
     if (!sessionCookie) {
         // No session found, redirect to login page
-        redirect('/login');
+        redirect("/login");
     }
 
     const payload = await decrypt(sessionCookie);
 
     if (!payload) {
         // Session expired or invalid, redirect to login page
-        redirect('/login');
+        redirect("/login");
     }
 
-    const user: User | null = await prisma.user.findUnique({
-        where: { id: payload.userId },
-    })
+    const user =
+        typeof payload.userId === "number"
+            ? await prisma.user.findUnique({ where: { id: payload.userId } })
+            : null;
 
     if (!user) {
-        // User not found, redirect to login page
-        redirect('/login');
-    } 
+        redirect("/login");
+    }
 
     return (
         <div>
-            <HomePage user={user}/>
-            {/* <h1>Welcome back, {user.firstName}</h1> */}
+            <HomePage user={user} />
         </div>
-    )
+    );
 }
