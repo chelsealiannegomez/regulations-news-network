37ed09229a45244ec1adce1ce87194cc637e89d3
Chelsea Lianne Tan Gomez <chelseagomez@meta.com>
2025-07-09 11:29:48 -0700

    hard-coded colors to be compatible with dark mode

--- nextjs/app/components/home/HomePage.tsx
+++ nextjs/app/components/home/HomePage.tsx
@@ -6,7 +6,7 @@ import type { HomePageProps } from "@/lib/types";
 export default function HomePage({ user }: HomePageProps) {
     return (
         <div>
-            <div>
+            <div className="text-black">
                 <Hero user={user} />
                 <ArticleSection user={user} />
             </div>
