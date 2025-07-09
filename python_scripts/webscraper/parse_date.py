fde9c2477edfb2713776ee6f87c9f2acb5053dbf
Chelsea Lianne Tan Gomez <chelseagomez@meta.com>
2025-07-09 11:06:39 -0700

    changed db to date for easy weighting in recommendation model

---
+++ python_scripts/webscraper/parse_date.py
@@ -0,0 +1,23 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+def parse_date_DMY(date):
+    dateList = date.split(" ")
+    
+    day = int(dateList[0])
+    month = months.index(dateList[1])
+    year = int(dateList[2])
+
+    return year, month, day
