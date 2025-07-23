75a881bc1217287d2a6aba32d2e4c1a11c68785b
Chelsea Lianne Tan Gomez <ctgomez@asu.edu>
2025-07-23 00:00:14 -0700

    changed stop words to import from nltk

--- python_scripts/recommendation_model/vectorizer/infer_article_embedding.py
+++ python_scripts/recommendation_model/vectorizer/infer_article_embedding.py
@@ -1,12 +1,14 @@
 import json
 import tensorflow as tf
 import numpy as np
+from nltk.corpus import stopwords
 
 from create_model import ReduceMeanLayer
 
+stop_words = set(stopwords.words('english'))
+
 def remove_stop_words(content):
     # content: str[] (array of paragraphs)
-    stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
     results = []
     for text in content:
