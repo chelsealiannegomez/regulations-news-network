8c8a3023a4289ffe3c2cf77548a09863a210e83a
Chelsea Lianne Tan Gomez <ctgomez@asu.edu>
2025-07-23 00:21:23 -0700

    made everything functions for easier testinG

--- python_scripts/recommendation_model/save_embeddings_to_db.py
+++ python_scripts/recommendation_model/save_embeddings_to_db.py
@@ -6,29 +6,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-doc_embeddings = np.load('doc_embeddings.npy')
-
 def get_doc_ids():
     with open('doc_ids.json', 'r') as f:
         doc_ids = json.load(f)
 
     return doc_ids
 
-doc_ids = get_doc_ids()
+def connect_to_db():
 
-host = os.getenv("PGHOST")
-conn_string = f"host={host} dbname={os.getenv("PGDATABASE")} user={os.getenv("PGUSER")} password={os.getenv("PGPASSWORD")}"
+    host = os.getenv("PGHOST")
+    conn_string = f"host={host} dbname={os.getenv("PGDATABASE")} user={os.getenv("PGUSER")} password={os.getenv("PGPASSWORD")}"
 
-conn = psycopg2.connect(conn_string)
+    conn = psycopg2.connect(conn_string)
 
-cursor = conn.cursor()
+    cursor = conn.cursor()
 
+    return conn, cursor
 
-for idx in range(len(doc_embeddings)):
-    cursor.execute("""
-    INSERT INTO embeddings (article_id, vector)
-    VALUES (%s, %s);
-    """, (doc_ids[str(idx)], doc_embeddings[idx].tolist()))
+def save_embeddings():
+    doc_embeddings = np.load('doc_embeddings.npy')
 
-    conn.commit()
-    print(f"Article {doc_ids[str(idx)]} added to db")
+    doc_ids = get_doc_ids()
+    conn, cursor = connect_to_db()
+    for idx in range(len(doc_embeddings)):
+        cursor.execute("""
+        INSERT INTO embeddings (article_id, vector)
+        VALUES (%s, %s);
+        """, (doc_ids[str(idx)], doc_embeddings[idx].tolist()))
+
+        conn.commit()
+        print(f"Article {doc_ids[str(idx)]} added to db")
